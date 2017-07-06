7445f79d9cb007fa8d846ddfb235adef45802b6d
Kat Downes <katdownes@Kats-MacBook-Pro.local>
2017-07-06 13:08:36 +0100

    step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  # it 'responds to release_bike method' do
+  #   expect(subject).to respond_to(:release_bike)
+#  it { is_expected.to respond_to(:release_bike)}
+  it { should respond_to(:release_bike)}
+  
 end
