17a1a8da9b444d62570fb1d4093c9038db69811a
Alison Woodman <aliwoodman@gmail.com>
2017-07-07 14:51:51 +0100

    Step 15 - methods refactored to apply SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,11 +8,21 @@ class DockingStation
   end
 
   def release_bike
-      @bikes == [] ? fail("I do apologise, there are no bikes available") : @bikes.shift
+       empty? ? fail("I do apologise, there are no bikes available") : @bikes.shift
   end
 
   def dock bike
-      @bikes.length == 20 ? fail("I do apologise, this station is full") : @bikes << bike
+      full? ? fail("I do apologise, this station is full") : @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.length == 20
+  end
+
+  def empty?
+    @bikes == []
   end
 
 end
